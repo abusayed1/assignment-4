7ae896c38660a14c89fa92760a28a12fe7cec1ad
abusayed1 <mdabusayed72933sayed@gmail.com>
2022-08-11 13:17:24 +0600

    first comment

---
+++ assignment.js
@@ -0,0 +1,58 @@
+// problem slove-1: convert radian To Degree
+
+// function radianToDegree(radian){
+//    let piValue=3.1416;
+//    let inputType=typeof(radianInput);
+//   if (inputType==='number'){
+//     const degree=radian*180/piValue;
+//     const degreeValue=degree.toFixed(2);
+//     return degreeValue;
+//   }
+//  else{
+//   return 'error:-"correct your input Type.Only number is allowed."'
+//  }   
+// }
+// const radianInput='a';
+// const radianValue=radianToDegree(radianInput);
+// console.log(radianValue);
+   
+
+// problem slove-2: identify is it JavaScriptFile 
+
+// function isJavaScriptFile(name){
+//   if(typeof(fillName)==='string'){
+//     const fillType=fillName.endsWith('.js');
+//     return fillType;
+//   }
+//   else{
+//     return 'error-correct your fillname';
+//   }
+// }
+// const fillName='pg.js';
+// const parameterInput=isJavaScriptFile(fillName);
+// console.log(parameterInput);
+
+
+// problem slove-3: calculating totall oilprice.
+
+// function oilPrice(parameter){
+// if(typeof(parameter)==='number'){
+//     const dieselPricePerLitter=114;
+//     const petrolPricePerLitter=130;
+//     const octanePricePerLitter=135;
+//  const totallDieselPrice=dieselPricePerLitter*dieselQuentity;
+//  const totallPetrolPrice=petrolPricePerLitter*petrolQuentity;
+//  const totallOctanePrice=octanePricePerLitter*octaneQuentity;
+//  const totallCost=totallDieselPrice+totallOctanePrice+totallOctanePrice;
+//  return totallCost;
+// }
+// else{
+//     return 'error- correct your input.';
+// }
+// }
+// let dieselQuentity=0;
+// let petrolQuentity=2;
+// let octaneQuentity=3;
+// const oilpriceParameter=oilPrice(dieselQuentity,petrolQuentity,octaneQuentity);
+// console.log(oilpriceParameter);
+
